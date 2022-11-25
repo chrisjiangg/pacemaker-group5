fe399547c76dd9a4b448bb407804ec8728c817b1
Xingyu-Alexander-Liu <liu175@mcmaster.ca>
2022-11-24 20:01:48 -0500

    change vrp to 0.5

---
+++ Simulink Assignment 2/VVI_prev_ert_rtw/VVI_prev_data.c
@@ -0,0 +1,54 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: VVI_prev_data.c
+ *
+ * Code generated for Simulink model 'VVI_prev'.
+ *
+ * Model version                  : 1.12
+ * Simulink Coder version         : 9.7 (R2022a) 13-Nov-2021
+ * C/C++ source code generated on : Thu Nov 24 18:40:20 2022
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: ARM Compatible->ARM Cortex
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#include "VVI_prev.h"
+
+/* Block parameters (default storage) */
+P_VVI_prev_T VVI_prev_P = {
+  /* Expression: SampleTime
+   * Referenced by: '<S2>/VENT_CMP_DETECT'
+   */
+  -1.0,
+
+  /* Expression: 3
+   * Referenced by: '<S2>/PULSE_AMPLITUDE'
+   */
+  3.0,
+
+  /* Expression: 0.4
+   * Referenced by: '<S2>/PULSE_WIDTH'
+   */
+  0.4,
+
+  /* Expression: 60
+   * Referenced by: '<S2>/LRL'
+   */
+  60.0,
+
+  /* Expression: 0.5
+   * Referenced by: '<S2>/VRP'
+   */
+  0.5
+};
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
